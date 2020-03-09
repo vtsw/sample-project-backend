92c5e6ede743df5ba6d954d426b0297212f571b0
Sam <sam.to@ewoosoft.com>
2020-03-09 16:58:37 +0700

    message resolver

--- modules/message/graphql/resolver.js
+++ modules/message/graphql/resolver.js
@@ -1,8 +1,15 @@
+const { isEmpty } = require('lodash');
+
 module.exports = {
   Query: {
     message: (_, { id }, { messageProvider }) => messageProvider.findById(id),
-    messageList: async (_, { query: { searchText, limit, skip } }, { messageProvider }) => {
-      const content = searchText ? new RegExp(`/\b${searchText}\b/`) : {};
+    messageList: async (_, args, { messageProvider }) => {
+      if (isEmpty(args)) {
+        return messageProvider
+          .findWithPagination({ query: { }, page: { limit: 10, skip: 0 } });
+      }
+      const { query: { searchText, limit, skip } } = args;
+      const content = isEmpty(searchText) ? new RegExp(`${searchText}`) : {};
       return messageProvider
         .findWithPagination({ query: { content }, page: { limit, skip } });
     },
--- modules/user/graphql/resolver.js
+++ modules/user/graphql/resolver.js
@@ -3,9 +3,15 @@ const { isEmpty } = require('lodash');
 module.exports = {
   Query: {
     user: (_, { id }, { userProvider }) => userProvider.findById(id),
-    userList: async (_, { query: { searchText, limit, skip } }, { userProvider }) => {
+    userList: async (_, args, { userProvider }) => {
+      if (isEmpty(args)) {
+        return userProvider
+          .findWithPagination({ query: { }, page: { limit: 10, skip: 0 } });
+      }
+      const { query: { searchText, limit, skip } } = args;
       const pattern = isEmpty(searchText) ? new RegExp(`${searchText}`) : {};
-      return userProvider.findWithPagination({ query: { $or: [{ name: pattern }, { email: pattern }] }, page: { limit, skip } });
+      return userProvider
+        .findWithPagination({ query: { $or: [{ name: pattern }, { email: pattern }] }, page: { limit, skip } });
     },
   },
   Mutation: {
