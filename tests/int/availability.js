731ebf57619354c1908b9c2f52d226bc861506f1
leo <leo.vo@ewoosoft.com>
2020-03-10 11:35:44 +0700

    add availability test

--- tests/int/availability.js
+++ tests/int/availability.js
@@ -1,22 +1,43 @@
-const serverConfig = require('../../config');
-const ApolloClient = require('apollo-boost');
-const gql = ApolloClient.gql;
-const serverPort = serverConfig.app.port;
+const gql = require('graphql-tag');
+const { ApolloClient } = require('apollo-client');
+const { InMemoryCache } = require('apollo-cache-inmemory');
+const { createHttpLink } = require('apollo-link-http');
+const { setContext } = require('apollo-link-context');
+const fetch = require("node-fetch");
+jest.mock('node-fetch', ()=>jest.fn())
+
+const httpLink =  createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  fetch
+});
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+    },
+  }})
 
 const TEST_QUERY = gql`
 {
-  userList {
+  query userList {
     users
   }
 }
 `;
-
+console.log(authLink)
 const client = new ApolloClient({
-  uri: `https://web:${serverPort}`,
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
-
-client
-  .query({
-    query: TEST_QUERY
+describe('Test availability', () => {
+  it("Should query users", () => {
+    client
+      .query({
+        query: TEST_QUERY
+      })
+      .then(
+        result =>
+        {console.log(result);})
+      .catch(error => console.log(error))
   })
-  .then(result => console.log(result));
+});
