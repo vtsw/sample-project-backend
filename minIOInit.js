19f46b472dc8553fceba4c31d9126ba30ae6b327
Sam <sam.to@ewoosoft.com>
2020-03-26 16:02:35 +0700

    init minIO

---
+++ minIOInit.js
@@ -0,0 +1,65 @@
+const config = require('./config');
+const minio = require('./services/minio');
+
+const minioClient = minio(config);
+
+const publicPolicy = {
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Effect: 'Allow',
+      Principal: '*',
+      Action: [
+        's3:GetObject',
+      ],
+      Resource: 'arn:aws:s3:::upload/*',
+    },
+    {
+      Effect: 'Allow',
+      Principal: {
+        AWS: [
+          '*',
+        ],
+      },
+      Action: [
+        's3:GetBucketLocation',
+        's3:ListBucket',
+        's3:ListBucketMultipartUploads',
+      ],
+      Resource: [
+        'arn:aws:s3:::upload',
+      ],
+    },
+    {
+      Effect: 'Allow',
+      Principal: {
+        AWS: [
+          '*',
+        ],
+      },
+      Action: [
+        's3:PutObject',
+        's3:AbortMultipartUpload',
+        's3:DeleteObject',
+        's3:GetObject',
+        's3:ListMultipartUploadParts',
+      ],
+      Resource: [
+        'arn:aws:s3:::upload/*',
+      ],
+    },
+  ],
+};
+
+(async () => {
+  const uploadExist = await minioClient.bucketExists('upload');
+  if (!uploadExist) {
+    await minioClient.makeBucket('upload');
+    minioClient.setBucketPolicy('upload', JSON.stringify(publicPolicy));
+  }
+  minioClient.bucketExists('logs').then((exist) => {
+    if (!exist) {
+      minioClient.makeBucket('logs');
+    }
+  });
+})().catch((e) => console.error(e));
