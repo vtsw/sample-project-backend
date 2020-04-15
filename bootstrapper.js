0ace4bb339f2bc23b20fe824a5f4d8fae98acaa7
Mike <EVN0031@VATECHNETWORKS.COM>
2020-04-15 14:02:28 +0700

    #customer add customer provider to bootraper

--- bootstrapper.js
+++ bootstrapper.js
@@ -4,6 +4,7 @@ const mongodb = require('./services/mongodb');
 const minio = require('./services/minio');
 const UserProvider = require('./modules/user/UserProvider');
 const MessageProvider = require('./modules/message/MessageProvider');
+const CustomerProvider = require('./modules/customer/CustomerProvider')
 const Bcrypt = require('./services/bcrypt');
 const JWT = require('./services/jwt');
 const config = require('./config');
@@ -19,6 +20,8 @@ module.exports = async () => {
   container.register({
     db: asValue(db),
     userProvider: asClass(UserProvider).inject((injectedContainer) => ({ users: injectedContainer.resolve('db').collection('users') })).singleton(),
+    cserProvider: asClass(CustomerProvider)
+      .inject((injectedContainer) => ({ customers: injectedContainer.resolve('db').collection('customers') })).singleton(),
     messageProvider: asClass(MessageProvider)
       .inject((injectedContainer) => ({ messages: injectedContainer.resolve('db').collection('messages') })).singleton(),
     config: asValue(config),
