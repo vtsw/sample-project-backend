3d938cb4d806230c2a69ecc176fc84aa22657f69
Mike <EVN0031@VATECHNETWORKS.COM>
2020-05-05 09:13:33 +0700

    fix upload image

--- modules/resolver.js
+++ modules/resolver.js
@@ -10,37 +10,44 @@ const baseResolver = {
   Mutation: {
     hello: (_, { name }) => `hello ${name}`,
     uploadImage: async (source, { file }, { container, req }) => {
-      const minio = container.resolve('minio');
-      const userProvider = container.resolve('userProvider');
-      const bucketName = 'upload';
-      const {
-        filename, mimetype, encoding, createReadStream,
-      } = await file;
+      try {
+        const minio = container.resolve('minio');
+        const userProvider = container.resolve('userProvider');
+        const bucketName = 'upload';
+        const {
+          filename, mimetype, encoding, createReadStream,
+        } = await file;
+        if (
+          !mimetype.includes('jpeg')
+          && !mimetype.includes('jpg')
+          && !mimetype.includes('png')
+          && !mimetype.includes('gif')
+        ) {
+          throw new Error('Only images are allowed');
+        }
+        const hashedFilename = `${uuidv4()}${filename.trim()}`;
 
-      if (
-        !mimetype.includes('jpeg')
-        && !mimetype.includes('jpg')
-        && !mimetype.includes('png')
-        && !mimetype.includes('gif')
-      ) {
-        throw new Error('Only images are allowed');
+        const etag = await minio.putObject(bucketName, hashedFilename, createReadStream(), { mimetype, encoding });
+
+        const url = new URL(`${config.app.host}`);
+        url.port = config.app.port;
+
+        const { user: loggedUser } = req;
+
+        url.pathname = `/api/download/images/${hashedFilename}`;
+        const image = {
+          filename,
+          hashedFilename,
+          mimetype,
+          encoding,
+          link: url.href,
+          etag,
+        };
+        await userProvider.update(loggedUser.id, { image });
+        return image;
+      } catch (err) {
+        console.log(err);
       }
-      const hashedFilename = `${uuidv4()}${filename.trim()}`;
-      const etag = await minio.putObject(bucketName, hashedFilename, createReadStream(), { mimetype, encoding });
-      const url = new URL(`${config.app.host}`);
-      url.port = config.app.port;
-      const { user: loggedUser } = req;
-      url.pathname = `/api/download/images/${hashedFilename}`;
-      const image = {
-        filename,
-        hashedFilename,
-        mimetype,
-        encoding,
-        link: url.href,
-        etag,
-      };
-      await userProvider.update(loggedUser.id, { image });
-      return image;
     },
   },
   Query: {
