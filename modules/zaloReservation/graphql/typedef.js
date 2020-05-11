f55ca67139a281a79a65b5b8f9bb8fd2d8f47a05
Mike <EVN0031@VATECHNETWORKS.COM>
2020-05-11 09:49:09 +0700

    reservation

---
+++ modules/zaloReservation/graphql/typedef.js
@@ -0,0 +1,26 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+    type bookingOptions {
+      doctor: String!,
+      time: String!,
+    }
+
+    input ReservationInput {
+      patient: String!,
+      bookingOptions: [bookingOptions!]!
+    }
+    
+    type Query {
+      reservation: String
+    }
+
+    type Mutation {
+      sendExamimationReservationMessage(reservation: ReservationInput!): String
+    }
+
+    input ZaloInterestedUserListInput {
+        limit: Int = 10
+        skip: Int = 0
+    }
+`;
