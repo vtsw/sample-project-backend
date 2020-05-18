001d173110d5cb75e2bfdf969ca9db6571cc0f9b
Mike <mike.bui@ewoosoft.com>
2020-05-18 15:43:44 +0700

    #resolve-conflic middleware/index.js

--- modules/middlewares/index.js
+++ modules/middlewares/index.js
@@ -1,6 +1,7 @@
 const validation = require('./validation');
 const logging = require('./logging');
 const errorHandler = require('./errorHandler');
+const mutationRecording = require('./mutationRecording');
 
 module.exports = {
   errorHandler,
