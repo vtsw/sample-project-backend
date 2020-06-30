7d81f247fac9c74ab72377afff7f8c0f3d6eba33
leovo178 <leo.vo@ewoosoft.com>
2020-06-30 18:13:26 +0700

    fix mismatch return field for zalomessage.id

--- modules/zaloOfficialAccount/Authenticator.js
+++ modules/zaloOfficialAccount/Authenticator.js
@@ -16,10 +16,12 @@ class Authenticator {
    */
   verify(token) {
     try {
+      console.log('jwt', this.jwt);
       const decodedToken = this.jwt.decode(token);
       console.log('decodedToken', decodedToken);
       return decodedToken;
     } catch (err) {
+      console.log(err);
       throw new AuthenticationError('Invalid token', err);
     }
   }
